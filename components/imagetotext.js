c8337f7f01b7bf565c82bcfdefe28ddc2267960f
dahyun lee <lee.dahyun@gmail.com>
2021-02-01 18:13:43 -0500

    image to text and text to translation working

--- components/imagetotext.js
+++ components/imagetotext.js
@@ -48,3 +48,38 @@ _convertToText = async (photo) => {
     console.error("An error occurred during text conversion:", err);
   }
 };
+
+const _convertToText = async (photo) => {
+  try {
+    let response = await fetch(
+      "https://batuta.cognitiveservices.azure.com/vision/v3.1/analyze?visualFeatures=Categories,Description&details=Landmarks",
+      {
+        body: { url: photo.base64 },
+        headers: {
+          "Content-Type": "application/json",
+          "Ocp-Apim-Subscription-Key": "d9d4ce379b7e4dc9b90addc50ecfeed0",
+        },
+        method: "POST",
+      }
+    );
+    console.log(response);
+    const responseJSON = await response.json();
+    if (
+      !(
+        responseJSON &&
+        responseJSON.responses &&
+        responseJSON.responses[0] &&
+        responseJSON.responses[0].fullTextAnnotation
+      )
+    ) {
+      Alert.alert(
+        "There was no readable text in your image. Please try again."
+      );
+    } else {
+      const text = responseJSON.responses[0].fullTextAnnotation.text;
+      console.log(text);
+    }
+  } catch (err) {
+    console.error("An error occurred during text conversion:", err);
+  }
+};
