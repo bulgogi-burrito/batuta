62757c9043664565de4b65e626f4e77b10ef529c
Kevin Minutti <minutti70@gmail.com>
2021-02-04 12:56:10 -0500

    removed permissions

--- components/camera.js
+++ components/camera.js
@@ -2,17 +2,17 @@ import * as ImagePicker from "expo-image-picker";
 import React from "react";
 import { Button, Image, StyleSheet, Text, View } from "react-native";
 import { connect } from "react-redux";
-import {setTranslation} from '../store/text'
-import TranslatedText from './translatedText' 
-import { callGoogleVision , callGoogleTranslate } from './google'
-import Permissions from './permissions'
+import { setTranslation } from "../store/text";
+import TranslatedText from "./translatedText";
+import { callGoogleVision, callGoogleTranslate } from "./google";
+// import Permissions from './permissions'
 
 function Camera(props) {
-  const {cameraPermission} = props ; 
+  // const {cameraPermission} = props ;
   const [image, setImage] = React.useState(null);
   const [status, setStatus] = React.useState(null);
   const [result, setResult] = React.useState(null);
-  
+
   const takePictureAsync = async () => {
     const { cancelled, uri, base64 } = await ImagePicker.launchCameraAsync({
       base64: true,
@@ -22,42 +22,50 @@ function Camera(props) {
       setImage(uri);
       setStatus("Loading...");
       try {
-        const { sourceLang, targetLang , setTranslation } = props;
+        const { sourceLang, targetLang, setTranslation } = props;
         const textFromImage = await callGoogleVision(base64);
-        const translatedResult = await callGoogleTranslate(textFromImage,sourceLang,targetLang)
-        await setTranslation(textFromImage,translatedResult)
-        setResult(translatedResult) ; 
-        setStatus('Done');
+        const translatedResult = await callGoogleTranslate(
+          textFromImage,
+          sourceLang,
+          targetLang
+        );
+        await setTranslation(textFromImage, translatedResult);
+        setResult(translatedResult);
+        setStatus("Done");
       } catch (error) {
         setStatus(`Error: ${error.message}`);
       }
     } else {
       setImage(null);
       setStatus(null);
-      setResult(null); 
+      setResult(null);
     }
   };
 
-  if ( status === 'Loading...') return (<View> 
-    <Text>Making Translation</Text> 
-  </View>)
-  else if ( status === 'Done' && result ) return (<TranslatedText result={result}  />)
-  else return (
-    <View style={styles.container}>
-      {cameraPermission === false ? (
+  if (status === "Loading...")
+    return (
+      <View>
+        <Text>Making Translation</Text>
+      </View>
+    );
+  else if (status === "Done" && result)
+    return <TranslatedText result={result} />;
+  else
+    return (
+      <View style={styles.container}>
+        {/* {cameraPermission === false ? (
          <>
          <Permissions type={'camera'} />
          </>
-      ) : (
+      ) : ( */}
         <>
           {image && <Image style={styles.image} source={{ uri: image }} />}
           <Button onPress={takePictureAsync} title="Take a Picture" />
         </>
-      )}
-    </View>
-  );
+        {/* )} */}
+      </View>
+    );
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -79,14 +87,15 @@ const mapState = (state) => {
   return {
     sourceLang: state.language.sourceLang,
     targetLang: state.language.targetLang,
-    cameraPermission : state.permissions.cameraPermission
+    cameraPermission: state.permissions.cameraPermission,
   };
 };
 
 const mapDispatch = (dispatch) => {
   return {
-    setTranslation : (originalText,translatedText)  => dispatch(setTranslation(originalText,translatedText))
-  }
-}
+    setTranslation: (originalText, translatedText) =>
+      dispatch(setTranslation(originalText, translatedText)),
+  };
+};
 
-export default connect(mapState,mapDispatch)(Camera);
+export default connect(mapState, mapDispatch)(Camera);
