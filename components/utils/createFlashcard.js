693feebd6eef148f53ee52f4aa6c258afc4002af
Kevin Minutti <minutti70@gmail.com>
2021-02-09 22:36:00 -0500

    work on flashcards

--- components/utils/createFlashcard.js
+++ components/utils/createFlashcard.js
@@ -24,3 +24,46 @@ export default createFlashcard;
 //
 //
 //
+// import React, { useState } from "react";
+// import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+// import { db } from "../db/index"; //importing database
+
+// export default function Flashcards() {
+//   const [flashcards, setFlashcards] = React.useState(null);
+
+//   React.useEffect(() => {
+//     db.transaction((tx) => {
+//       tx.executeSql(
+//         `select * from flashcards;`,
+//         null,
+//         (_, { rows: { _array } }) => setFlashcards(_array)
+//       );
+//     });
+//   }, []);
+
+//   return (
+//     <View>
+//       {flashcards &&
+//         flashcards.map(({ id, input_text }) => (
+//           <TouchableOpacity
+//             key={id}
+//             // onPress={() =>}
+//             style={{
+//               borderColor: "#000",
+//               borderWidth: 1,
+//               padding: 8,
+//             }}
+//           >
+//             <Text>{input_text}</Text>
+//           </TouchableOpacity>
+//         ))}
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     backgroundColor: "#fff",
+//     flex: 1,
+//   },
+// });
